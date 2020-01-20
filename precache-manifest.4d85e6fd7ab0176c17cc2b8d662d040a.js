fdf81ee85cd51e1685f16c10b3714c7517caaaa0
YishengSun <yisheng4@illinois.edu>
2020-01-20 15:55:24 -0600

    Updates

---
+++ precache-manifest.4d85e6fd7ab0176c17cc2b8d662d040a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e74126f8ecfd5a7349a3c05ec533b7c",
+    "url": "/Jokes-App/index.html"
+  },
+  {
+    "revision": "7f8510b6fd78a20312e1",
+    "url": "/Jokes-App/static/css/main.ce789ef7.chunk.css"
+  },
+  {
+    "revision": "771a304cf1bd66554041",
+    "url": "/Jokes-App/static/js/2.40ce0560.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Jokes-App/static/js/2.40ce0560.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f8510b6fd78a20312e1",
+    "url": "/Jokes-App/static/js/main.810bb56a.chunk.js"
+  },
+  {
+    "revision": "a612c9cb72f9817066f3",
+    "url": "/Jokes-App/static/js/runtime-main.1f54d02f.js"
+  }
+]);
